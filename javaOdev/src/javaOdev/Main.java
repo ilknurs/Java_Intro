bed71c806d7e73a59b5877e2d0e4a78cc05c668b
ilknurs <ilknursercek4@gmail.com>
2021-04-30 02:28:49 +0300

    2.gün Ödev1

--- javaOdev/src/javaOdev/Main.java
+++ javaOdev/src/javaOdev/Main.java
@@ -3,8 +3,17 @@ package javaOdev;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Course course1 = new Course("C# - Angular", "Yazılım Geliştirici Yetiştirme Kampı");
+		Course course2 = new Course("Java - React", "Yazılım Geliştirici Yetiştirme Kampı");
+
+		Course[] courses = { course1, course2 };
+		for (Course course : courses) {
+			System.out.println("Kurs Adı: " + course.name);
+		}
+
+		CourseManager courseManager = new CourseManager();
+		courseManager.add(course1);
 
 	}
-
 }
